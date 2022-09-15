5c165016cb1461d90073063f36a642a8c70611cd
Omri Webber <omriwebber@gmail.com>
2022-09-15 16:40:50 +1200

    update UI

--- static/js/main.js
+++ static/js/main.js
@@ -24,10 +24,30 @@ $(document).on("click", ".delete-contact-button", function () {
     var contactfName = $(this).data('fname');
     var contactlName = $(this).data('lname');
     var contactID = $(this).data('id');
-    console.log(contactName)
     document.getElementById("contactName").innerHTML = contactfName + " " + contactlName;
     document.getElementById("confirm-delete-button").setAttribute("href", "/deleteContact/" + contactID);
-    // As pointed out in comments, 
-    // it is unnecessary to have to manually call the modal.
-    // $('#addBookDialog').modal('show');
+});
+
+function generateNewPasscode(){
+    var val = Math.floor(1000 + Math.random() * 9000);
+    var seq = (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
+    document.getElementById("passcode").value = seq;
+}
+
+const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+
+const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+    const table = th.closest('table');
+    Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+        .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+        .forEach(tr => table.appendChild(tr) );
+})));
+
+$( document ).ready(function() {
+    user_name = $('.user-name').text().slice(1,2);
+    $('.user-letter').text(user_name.toUpperCase());
 });
